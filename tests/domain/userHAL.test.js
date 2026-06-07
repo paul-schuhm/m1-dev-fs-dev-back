ffe17f468f3b9951fd2023dec4b78b56cef980a7
paul schuhmacher <contact@pschuhmacher.com>
2026-06-07 07:53:28 +0200

    mise a jour

--- tests/domain/userHAL.test.js
+++ tests/domain/userHAL.test.js
@@ -1,14 +1,15 @@
 const hal = require('../../src/service/hal');
 
+
 describe('=== TEST DOMAINE : Formatage HAL d\'un Utilisateur ===', () => {
 
     it('devrait transformer un utilisateur brut en un Resource Object HAL valide', () => {
         const fakeUserData = {
             id: 1,
-            pseudo: "pocky_dev",
-            email: "pocky@example.com"
+            pseudo: 'pocky_dev',
+            email: 'pocky@example.com',
         };
-        const baseUrl = "/v1";
+        const baseUrl = '/v1';
 
         // WHEN : On applique la fonction pure du domaine
         const result = hal.userToResourceObject(fakeUserData, baseUrl);
@@ -17,13 +18,13 @@ describe('=== TEST DOMAINE : Formatage HAL d\'un Utilisateur ===', () => {
         
         // 1. Validation de l'encapsulation de l'identité (_embedded)
         expect(result).toHaveProperty('_embedded');
-        expect(result._embedded.pseudo).toBe("pocky_dev");
+        expect(result._embedded.pseudo).toBe('pocky_dev');
 
         // 2. Validation des hypermédias (_links) et de la construction de l'URL
         expect(result).toHaveProperty('_links');
         expect(result._links).toHaveProperty('self');
         
         // On vérifie que le pseudo est bien injecté à la fin de l'URL de l'arborescence
-        expect(result._links.self.href).toBe("/v1/utilisateurs/pocky_dev");
+        expect(result._links.self.href).toBe('/v1/utilisateurs/pocky_dev');
     });
 });
--- tests/http/concerts.test.js
+++ tests/http/concerts.test.js
@@ -1,12 +1,20 @@
 const request = require('supertest');
 const express = require('express');
-const concertController = require('../../src/controller/v1/concertController');
 
-// 1. On mock le repository pour qu'il renvoie des données prévisibles en dur
+//Mock la database
+jest.mock('../../src/data/db.js', () => ({
+    connexion: {
+        execute: jest.fn().mockResolvedValue([[]]),
+    },
+}));
+
+const concertController = require('../../src/controller/v1/concertController');
 const concertRepository = require('../../src/repository/concertRepository');
+
+//Mock le concertRepository
 jest.mock('../../src/repository/concertRepository');
 
-// 2. On crée une mini-app Express jetable dédiée au test du contrôleur
+//On crée une mini-app Express jetable dédiée au test du contrôleur
 const app = express();
 app.get('/v1/concerts', concertController.all);
 
@@ -17,12 +25,11 @@ describe('=== TEST HTTP : concertController ===', () => {
 
     it('devrait répondre avec le bon Content-Type et un statut 200', async () => {
     // On prépare le comportement de notre mock
-        concertRepository.all.mockReturnValue([
+        concertRepository.upcomingConcerts.mockReturnValue([
             { id: 1, title: 'Futur Concert', date: '2028-01-01' },
         ]);
 
         const response = await request(app).get('/v1/concerts').expect(200);
-
         // Validation des attentes HTTP spécifiques
         expect(response.headers['content-type']).toMatch(/application\/hal\+json/);
         expect(response.body).toHaveProperty('_links');
@@ -30,6 +37,6 @@ describe('=== TEST HTTP : concertController ===', () => {
 
     it('devrait lever une erreur ou appeler next() si l\'offset est négatif', async () => {
         await request(app).get('/v1/concerts?offset=-5');
-        expect(concertRepository.all).not.toHaveBeenCalled();
+        expect(concertRepository.upcomingConcerts).not.toHaveBeenCalled();
     });
 });
